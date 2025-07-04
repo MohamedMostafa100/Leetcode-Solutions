8f985fb0665a1dffb35d8c7c59a37ff4d1cda21c
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-07-04 03:41:41 +0300

    Time: 329 ms (73.15%), Space: 175.1 MB (60.73%) - LeetHub

--- 3618-find-the-original-typed-string-ii/3618-find-the-original-typed-string-ii.cpp
+++ 3618-find-the-original-typed-string-ii/3618-find-the-original-typed-string-ii.cpp
@@ -1,48 +1,54 @@
 class Solution {
 public:
     int possibleStringCount(string word, int k) {
-        int n = word.size(), cnt = 1;
-        vector<int> freq;
-        for (int i = 1; i < n; ++i) {
+        vector<int> freqs;
+        int count = 1;
+        long long total = 1;
+        long long invalid = 0;
+        for (int i = 1; i < word.length(); i++) {
             if (word[i] == word[i - 1]) {
-                ++cnt;
+                count++;
             } else {
-                freq.push_back(cnt);
-                cnt = 1;
+                freqs.push_back(count);
+                count = 1;
             }
         }
-        freq.push_back(cnt);
+        freqs.push_back(count);
 
-        int ans = 1;
-        for (int o : freq) {
-            ans = static_cast<long long>(ans) * o % mod;
+        for (int i = 0; i < freqs.size(); i++) {
+            total = (total * freqs[i]) % 1000000007;
         }
 
-        if (freq.size() >= k) {
-            return ans;
+        if (freqs.size() >= k) {
+            return total;
         }
 
-        vector<int> f(k), g(k, 1);
-        f[0] = 1;
-        for (int i = 0; i < freq.size(); ++i) {
-            vector<int> f_new(k);
-            for (int j = 1; j < k; ++j) {
-                f_new[j] = g[j - 1];
-                if (j - freq[i] - 1 >= 0) {
-                    f_new[j] = (f_new[j] - g[j - freq[i] - 1] + mod) % mod;
+        vector<int> dp(k, 0);
+        dp[0] = 1;
+
+        for (int i = 0; i < freqs.size(); i++) {
+            vector<int> new_dp(k, 0);
+            long long sum = 0;
+            for (int j = 0; j < k; j++) {
+                if (j > 0) {
+                    sum = (sum + dp[j - 1]) % 1000000007;
                 }
-            }
-            vector<int> g_new(k);
-            g_new[0] = f_new[0];
-            for (int j = 1; j < k; ++j) {
-                g_new[j] = (g_new[j - 1] + f_new[j]) % mod;
-            }
-            f = move(f_new);
-            g = move(g_new);
-        }
-        return (ans - g[k - 1] + mod) % mod;
-    }
 
-private:
-    static const int mod = 1000000007;
+                if (j > freqs[i]) {
+                    sum =
+                        (sum - dp[j - freqs[i] - 1] + 1000000007) % 1000000007;
+                }
+                new_dp[j] = sum;
+            }
+
+            dp = new_dp;
+        }
+        
+        for(int i = 0; i < k; i++)
+        {
+            invalid = (invalid + dp[i]) % 1000000007;
+        }
+
+        return (total - invalid + 1000000007) % 1000000007;
+    }
 };
