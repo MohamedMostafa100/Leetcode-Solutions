c299fbabc424008722a6c6dc41ff99860e87fd8b
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-07-14 03:29:57 +0300

    Time: 0 ms (100%), Space: 10.5 MB (71.97%) - LeetHub

--- 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -14,7 +14,7 @@ public:
         int res = 0;
         while(head)
         {
-            res = res * 2 + head->val;
+            res = (res << 1) + head->val;
             head = head->next;
         }
         return res;
