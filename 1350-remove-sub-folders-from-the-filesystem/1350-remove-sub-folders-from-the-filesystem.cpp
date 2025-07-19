4ed657ee7051ba80c603be132e7f76a13a1b96bb
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-07-20 01:43:38 +0300

    Time: 139 ms (28.88%), Space: 71.3 MB (40.13%) - LeetHub

---
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        vector<string> res;
+        unordered_set<string> paths(folder.begin(), folder.end());
+        for(int i = 0; i < folder.size(); i++)
+        {
+            string s = "";
+            for(int j = 1; j < folder[i].length(); j++)
+            {
+                s += '/';
+                while(folder[i][j] != '/' && j < folder[i].length())
+                {
+                    s += folder[i][j];
+                    j++;
+                }
+                if(paths.find(s) != paths.end() && s.length() != folder[i].length())
+                {
+                    paths.erase(folder[i]);
+                }
+            }
+        }
+        res.assign(paths.begin(), paths.end());
+        return res;
+    }
+};
