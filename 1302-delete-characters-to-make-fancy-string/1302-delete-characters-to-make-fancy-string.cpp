dd826c4c45ec554f1cb6ddb52e7fd317f2558580
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-07-21 18:36:30 +0300

    Time: 14 ms (95.81%), Space: 44.6 MB (39.72%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -3,14 +3,19 @@ public:
     string makeFancyString(string s) {
         string res = "";
         res += s[0];
-        if(s.length() > 1)
-        {
-            res += s[1];
-        }
-        for(int i = 2; i < s.length(); i++){
-            if(s[i] == s[i - 1] && s[i - 1] == s[i - 2])
+        int vioCount = 0;
+        for(int i = 1; i < s.length(); i++){
+            if(s[i] == s[i - 1])
             {
-                continue;
+                vioCount++;
+                if(vioCount >= 2)
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                vioCount = 0;
             }
             res += s[i];
         }
