d0d0b97985bb22048176614ebfb2722f4cc18b00
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-08-04 05:58:09 +0300

    Time: 4 ms (72.36%), Space: 124.4 MB (96.34%) - LeetHub

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
     int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
-        int left = 0;
-        int right = 0;
-        int n = fruits.size();
+        int res = 0;
+        int l = 0;
+        int r = 0;
         int sum = 0;
-        int ans = 0;
-
-        auto step = [&](int left, int right) -> int {
-            if (fruits[right][0] <= startPos) {
-                return startPos - fruits[left][0];
-            } else if (fruits[left][0] >= startPos) {
-                return fruits[right][0] - startPos;
-            } else {
-                return min(abs(startPos - fruits[right][0]),
-                           abs(startPos - fruits[left][0])) +
-                       fruits[right][0] - fruits[left][0];
+        for(int r = 0; r < fruits.size(); r++)
+        {
+            sum += fruits[r][1];
+            while(l <= r && steps(fruits, startPos, l, r) > k)
+            {
+                sum -= fruits[l][1];
+                l++;
             }
-        };
-        while (right < n) {
-            sum += fruits[right][1];
-            while (left <= right && step(left, right) > k) {
-                sum -= fruits[left][1];
-                left++;
-            }
-            ans = max(ans, sum);
-            right++;
+            res = max(res, sum);
         }
-        return ans;
+        return res;
+    }
+private:
+    int steps(vector<vector<int>>& fruits, int& startPos, int l, int r)
+    {
+        int res = 0;
+        if(fruits[r][0] <= startPos)
+        {
+            res = abs(startPos - fruits[l][0]);
+        }
+        else if(fruits[l][0] >= startPos)
+        {
+            res = abs(startPos - fruits[r][0]);
+        }
+        else
+        {
+            res = min(abs(startPos - fruits[l][0]), abs(startPos - fruits[r][0])) + fruits[r][0] - fruits[l][0];
+        }
+        return res;
     }
 };
