f96724f681f293a26aa65fc01374b7e6c5c2e1bc
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-08-06 02:53:22 +0300

    Time: 3 ms (49.54%), Space: 48.8 MB (74.59%) - LeetHub

---
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int res = 0;
+        for(int i = 0; i < fruits.size(); i++)
+        {
+            bool found = false;
+            for(int j = 0; j < baskets.size(); j++)
+            {
+                if(fruits[i] <= baskets[j])
+                {
+                    baskets[j] = -1;
+                    found = true;
+                    break;
+                }
+            }
+            if(!found)
+            {
+                res++;
+            }
+        }
+        return res;
+    }
+};
