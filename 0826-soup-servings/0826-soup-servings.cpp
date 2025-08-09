e6d126904af78a7c99f72bd5d52d29d86099bc9c
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-08-09 04:07:43 +0300

    Time: 0 ms (100%), Space: 9.8 MB (85.02%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.cpp
+++ 0826-soup-servings/0826-soup-servings.cpp
@@ -6,6 +6,7 @@ public:
         {
             return 1;
         }
+        n = ceil((double)n / 25);
         vector<vector<double>> dp(n + 1, vector<double>(n + 1, -1));
         res = solve(dp, n, n);
         return res;
@@ -15,6 +16,8 @@ private:
     {
         if(a <= 0 || b <= 0)
         {
+            a = max(a, 0);
+            b = max(b, 0);
             if(a < b)
             {
                 return 1;
@@ -28,10 +31,10 @@ private:
         if(dp[a][b] == -1)
         {
             dp[a][b] = 0;
-            dp[a][b] += 0.25 * solve(dp, a - 100, b);
-            dp[a][b] += 0.25 * solve(dp, a - 75, b - 25);
-            dp[a][b] += 0.25 * solve(dp, a - 50, b - 50);
-            dp[a][b] += 0.25 * solve(dp, a - 25, b - 75);
+            dp[a][b] += 0.25 * solve(dp, a - 4, b);
+            dp[a][b] += 0.25 * solve(dp, a - 3, b - 1);
+            dp[a][b] += 0.25 * solve(dp, a - 2, b - 2);
+            dp[a][b] += 0.25 * solve(dp, a - 1, b - 3);
         }
         return dp[a][b];
     }
