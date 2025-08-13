093ad61baa03288a2dbee569b7d20f5f0ea04e35
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-08-13 17:36:23 +0300

    Time: 6 ms (27.44%), Space: 8.9 MB (46.41%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        for(int i = 0; pow(3, i) <= n; i++)
+        for(int i = 0; i < 26; i++)
         {
             if(pow(3, i) == n)
             {
