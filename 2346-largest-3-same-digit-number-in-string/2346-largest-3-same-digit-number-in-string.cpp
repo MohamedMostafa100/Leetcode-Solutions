2c5d8a517eed7acd43ec7b7ef7750125d7948b23
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-08-15 22:03:52 +0300

    Time: 0 ms (100%), Space: 9.1 MB (22.97%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
@@ -1,28 +1,19 @@
 class Solution {
-    vector<string> sameDigitNumbers = {"999", "888", "777", "666", "555", "444", "333", "222", "111", "000"};
-
-    // Check whether the 'num' string contains the 'sameDigitNumber' string or not.
-    bool contains(string sameDigitNumber, string num) {
-        for (int index = 0; index <= num.size() - 3; ++index) {
-            if (num[index] == sameDigitNumber[0] and 
-                num[index + 1] == sameDigitNumber[1] and 
-                num[index + 2] == sameDigitNumber[2]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 public:
     string largestGoodInteger(string num) {
-        // Iterate on all 'sameDigitNumbers' and check if the string 'num' contains it.
-        for (auto& sameDigitNumber: sameDigitNumbers) {
-            if (contains(sameDigitNumber, num)) {
-                // Return the current 'sameDigitNumbers'.
-                return sameDigitNumber;
+        char largestNum = 0;
+        string goodStr = "";
+        for(int i = 0; i < num.length() - 2; i++)
+        {
+            if(num[i] == num[i + 1] && num[i + 1] == num[i + 2] && num[i] == num[i + 2])
+            {
+                if(goodStr.length() == 0 || num[i] - '0' > largestNum)
+                {
+                    goodStr = num.substr(i, 3);
+                    largestNum = num[i] - '0';
+                }
             }
         }
-        // No 3 consecutive same digits are present in the string 'num'.
-        return "";
+        return goodStr;
     }
 };
