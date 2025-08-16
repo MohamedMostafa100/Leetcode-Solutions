8d096e3358a8f939a8f624a64699edc914b29d66
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-08-17 00:31:08 +0300

    Time: 0 ms (100%), Space: 7.7 MB (99.58%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -3,15 +3,15 @@ public:
     int maximum69Number (int num) {
         int n = num;
         int toAdd = 0;
-        int i = 0;
+        int i = 1;
         while(n > 0)
         {
             if(n % 10 == 6)
             {
-                toAdd = 3 * pow(10, i);
+                toAdd = 3 * i;
             }
             n /= 10;
-            i++;
+            i *= 10;
         }
         return num + toAdd;
     }
