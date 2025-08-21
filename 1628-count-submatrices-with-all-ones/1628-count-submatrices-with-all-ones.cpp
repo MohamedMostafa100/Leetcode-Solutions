20500937bcaac4644b1a74a221f3b9ccfbcd7429
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-08-22 00:18:03 +0300

    Time: 8 ms (68.31%), Space: 19.7 MB (26.41%) - LeetHub

--- 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
     int numSubmat(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
         int res = 0;
-        vector<vector<int>> row(m, vector<int>(n, 0));
-
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (j == 0) {
-                    row[i][j] = mat[i][j];
-                } else {
-                    row[i][j] = (mat[i][j] == 0) ? 0 : row[i][j - 1] + 1;
+        vector<vector<int>> widths(mat.size(),
+                               vector<int>(mat[0].size(), 0));
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[i].size(); j++) {
+                if(mat[i][j] == 1)
+                {
+                    if(j == 0)
+                    {
+                        widths[i][j] = 1;
+                    }
+                    else
+                    {
+                        widths[i][j] = widths[i][j - 1] + 1;
+                    }
                 }
-                int cur = row[i][j];
-                for (int k = i; k >= 0; --k) {
-                    cur = min(cur, row[k][j]);
-                    if (cur == 0) {
+            }
+            for (int j = 0; j < mat[i].size(); j++) {
+                int minWidth = widths[i][j];
+                for(int k = i; k >= 0; k--)
+                {
+                    minWidth = min(minWidth, widths[k][j]);
+                    if(minWidth == 0)
+                    {
                         break;
                     }
-                    res += cur;
+                    res += minWidth;
                 }
             }
         }
+
         return res;
     }
 };
