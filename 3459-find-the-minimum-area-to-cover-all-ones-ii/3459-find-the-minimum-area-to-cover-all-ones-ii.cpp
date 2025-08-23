366c1929e50cb400133c41888b13916dcda14c88
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-08-24 01:31:13 +0300

    Time: 167 ms (9.89%), Space: 26 MB (18.68%) - LeetHub

--- 3459-find-the-minimum-area-to-cover-all-ones-ii/3459-find-the-minimum-area-to-cover-all-ones-ii.cpp
+++ 3459-find-the-minimum-area-to-cover-all-ones-ii/3459-find-the-minimum-area-to-cover-all-ones-ii.cpp
@@ -1,59 +1,70 @@
 class Solution {
-    int minimumSum(vector<vector<int>> &grid, int u, int d, int l, int r) {
-        int min_i = grid.size(), max_i = 0;
-        int min_j = grid[0].size(), max_j = 0;
-        for (int i = u; i <= d; i++) {
-            for (int j = l; j <= r; j++) {
-                if (grid[i][j] == 1) {
-                    min_i = min(min_i, i);
-                    min_j = min(min_j, j);
-                    max_i = max(max_i, i);
-                    max_j = max(max_j, j);
-                }
+public:
+    int minimumSum(vector<vector<int>>& grid) {
+        vector<vector<int>> gridTransposed(grid[0].size(), vector<int>(grid.size()));
+        for(int i = 0; i < gridTransposed.size(); i++)
+        {
+            for(int j = 0; j < gridTransposed[i].size(); j++)
+            {
+                gridTransposed[i][j] = grid[j][i];
             }
         }
-        return min_i <= max_i ? (max_i - min_i + 1) * (max_j - min_j + 1)
-                              : INT_MAX / 3;
+        return min(solve(grid), solve(gridTransposed));
     }
-
-    vector<vector<int>> rotate(vector<vector<int>> &vec) {
-        int n = vec.size(), m = vec[0].size();
-        vector<vector<int>> ret(m, vector<int>(n));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                ret[m - j - 1][i] = vec[i][j];
+private:
+    int solve(vector<vector<int>>& grid)
+    {
+        int res = INT_MAX;
+        //3 Vertical Splits
+        for(int i = 0; i + 2 < grid[0].size(); i++)
+        {
+            for(int j = i + 1; j + 1 < grid[0].size(); j++)
+            {
+                int split1 = minimumArea(grid, 0, grid.size() - 1, 0, i);
+                int split2 = minimumArea(grid, 0, grid.size() - 1, i + 1, j);
+                int split3 = minimumArea(grid, 0, grid.size() - 1, j + 1, grid[0].size() - 1);
+                res = min(res, split1 + split2 + split3);
             }
         }
-        return ret;
-    }
-
-    int solve(vector<vector<int>> &grid) {
-        int n = grid.size(), m = grid[0].size();
-        int res = n * m;
-        for (int i = 0; i + 1 < n; i++) {
-            for (int j = 0; j + 1 < m; j++) {
-                res =
-                    min(res, minimumSum(grid, 0, i, 0, m - 1) +
-                                 minimumSum(grid, i + 1, n - 1, 0, j) +
-                                 minimumSum(grid, i + 1, n - 1, j + 1, m - 1));
-                res = min(res, minimumSum(grid, 0, i, 0, j) +
-                                   minimumSum(grid, 0, i, j + 1, m - 1) +
-                                   minimumSum(grid, i + 1, n - 1, 0, m - 1));
-            }
-        }
-        for (int i = 0; i + 2 < n; i++) {
-            for (int j = i + 1; j + 1 < n; j++) {
-                res = min(res, minimumSum(grid, 0, i, 0, m - 1) +
-                                   minimumSum(grid, i + 1, j, 0, m - 1) +
-                                   minimumSum(grid, j + 1, n - 1, 0, m - 1));
+        //1 Vertical and 2 Horizontal Splits
+        for(int i = 0; i + 1 < grid.size(); i++)
+        {
+            for(int j = 0; j + 1 < grid[0].size(); j++)
+            {
+                int split1 = minimumArea(grid, 0, grid.size() - 1, 0, j);
+                int split2 = minimumArea(grid, 0, i, j + 1, grid[0].size() - 1);
+                int split3 = minimumArea(grid, i + 1, grid.size() - 1, j + 1, grid[0].size() - 1);
+                res = min(res, split1 + split2 + split3);
+                split1 = minimumArea(grid, 0, i, 0, j);
+                split2 = minimumArea(grid, i + 1, grid.size() - 1, 0, j);
+                split3 = minimumArea(grid, 0, grid.size() - 1, j + 1, grid[0].size() - 1);
+                res = min(res, split1 + split2 + split3);
             }
         }
         return res;
     }
-
-public:
-    int minimumSum(vector<vector<int>> &grid) {
-        auto rgrid = rotate(grid);
-        return min(solve(grid), solve(rgrid));
+    int minimumArea(vector<vector<int>>& grid, int rowStart, int rowEnd, int colStart, int colEnd) {
+        pair<int, int> topLeft = {-1, -1};
+        pair<int, int> bottomRight = {-1, -1};
+        for(int i = rowStart; i <= rowEnd; i++)
+        {
+            for(int j = colStart; j <= colEnd; j++)
+            {
+                if(grid[i][j] == 1)
+                {
+                    if(topLeft.first == -1)
+                    {
+                        topLeft = {i, j};
+                        bottomRight = {i, j};
+                    }
+                    else
+                    {
+                        topLeft.second = min(topLeft.second, j);
+                        bottomRight = {i, max(bottomRight.second, j)};
+                    }
+                }
+            }
+        }
+        return (bottomRight.first - topLeft.first + 1) * (bottomRight.second - topLeft.second + 1);
     }
 };
