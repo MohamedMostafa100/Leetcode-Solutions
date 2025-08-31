5991450da98c65af1cea72332ab77e2ea76e5c80
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-08-31 10:10:07 +0300

    Time: 0 ms (100%), Space: 21.6 MB (91.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,48 +1,44 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<vector<vector<bool>>> search(10, vector<vector<bool>>(3, vector<bool>(9, false)));
-        
-        for(char i = 0; i < board.size(); i++)
-        {
-            for(char j = 0; j < board[0].size(); j++)
-            {
-                if(board[i][j] != '.')
-                {
-                    if(search[board[i][j] - 48][0][i])
-                    {
+        unsigned short mask = 0;
+        for (char i = 0; i < board.size(); i++) {
+            for (char j = 0; j < board.size(); j++) {
+                if (board[i][j] != '.') {
+                    if (mask & (1 << (board[i][j] - '0'))) {
                         return false;
                     }
-                    else
-                    {
-                        search[board[i][j] - 48][0][i] = true;
-                    }
-
-                    if(search[board[i][j] - 48][1][j])
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        search[board[i][j] - 48][1][j] = true;
-                    }
-
-                    char rowNum = (i - (i % 3)) / 3;
-                    char colNum = (j - (j % 3)) / 3;
-                    char blockNum = rowNum * 3 + colNum;
-
-                    if(search[board[i][j] - 48][2][blockNum])
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        search[board[i][j] - 48][2][blockNum] = true;
-                    }
+                    mask |= (1 << (board[i][j] - '0'));
                 }
             }
+            mask = 0;
         }
-
+        for (char i = 0; i < board.size(); i++) {
+            for (char j = 0; j < board.size(); j++) {
+                if (board[j][i] != '.') {
+                    if (mask & (1 << (board[j][i] - '0'))) {
+                        return false;
+                    }
+                    mask |= (1 << (board[j][i] - '0'));
+                }
+            }
+            mask = 0;
+        }
+        for (char i = 0; i < board.size(); i += 3) {
+            for (char j = 0; j < board.size(); j += 3) {
+                for (char k = i; k < i + 3; k++) {
+                    for (char l = j; l < j + 3; l++) {
+                        if (board[k][l] != '.') {
+                            if (mask & (1 << (board[k][l] - '0'))) {
+                                return false;
+                            }
+                            mask |= (1 << (board[k][l] - '0'));
+                        }
+                    }
+                }
+                mask = 0;
+            }
+        }
         return true;
     }
 };
