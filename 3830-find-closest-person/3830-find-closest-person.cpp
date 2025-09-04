94338b7e15b1148703b1dfc0d3e164b34d04adb3
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-09-04 03:31:50 +0300

    Time: 0 ms (100%), Space: 8.5 MB (55.11%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.cpp
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-        return abs(z - x) < abs(z - y) ? 1 : abs(z - x) > abs(z - y) ? 2 : 0;
+        unsigned char dist1 = abs(z - x);
+        unsigned char dist2 = abs(z - y);
+        return dist1 < dist2 ? 1 : dist1 > dist2 ? 2 : 0;
     }
 };
