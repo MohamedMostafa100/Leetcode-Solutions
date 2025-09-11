68cacf6bbb8e93bbcbf6be1f99f23e6cd33a1066
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-09-12 02:54:31 +0300

    Time: 51 ms (5.03%), Space: 18.3 MB (15.66%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     string sortVowels(string s) {
         string t = "";
-        map<char, int> vowels = {{'A', 0}, {'E', 0}, {'I', 0}, {'O', 0}, {'U', 0}, {'a', 0}, {'e', 0}, {'i', 0}, {'o', 0}, {'u', 0}};
+        string sortedVowels = "AEIOUaeiou";
+        unordered_map<char, int> vowels = {{'A', 0}, {'E', 0}, {'I', 0}, {'O', 0}, {'U', 0}, {'a', 0}, {'e', 0}, {'i', 0}, {'o', 0}, {'u', 0}};
         for(int i = 0; i < s.length(); i++)
         {
             if(vowels.find(s[i]) != vowels.end())
@@ -14,12 +15,12 @@ public:
         {
             if(vowels.find(s[i]) != vowels.end())
             {
-                for(auto& vowel : vowels)
+                for(int j = 0; j < sortedVowels.length(); j++)
                 {
-                    if(vowel.second > 0)
+                    if(vowels[sortedVowels[j]] > 0)
                     {
-                        t += vowel.first;
-                        vowel.second--;
+                        t += sortedVowels[j];
+                        vowels[sortedVowels[j]]--;
                         break;
                     }
                 }
