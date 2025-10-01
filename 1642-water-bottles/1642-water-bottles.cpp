973d964ea039eb9bc1aff7ad097d6af6e9d9ffe4
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-10-01 23:37:33 +0300

    Time: 0 ms (100%), Space: 7.8 MB (79.98%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int res = numBottles;
+        int empty = numBottles;
+        while(empty >= numExchange)
+        {
+            res += (empty / numExchange);
+            empty = (empty / numExchange) + (empty % numExchange);
+        }
+        return res;
+    }
+};
