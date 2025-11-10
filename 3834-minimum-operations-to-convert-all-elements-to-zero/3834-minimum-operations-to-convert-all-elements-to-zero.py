38bacd74ca2eed4517b9836c59c227f67d7e8951
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-11-10 20:55:09 +0200

    Time: 273 ms (29.38%), Space: 29.7 MB (90.63%) - LeetHub

--- 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.py
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.py
@@ -3,9 +3,12 @@ class Solution:
         res = 0
         stk = []
         for num in nums:
+            if num == 0:
+                stk = []
+                continue
             while len(stk) > 0 and num < stk[-1]:
                 stk.pop()
-            if (len(stk) == 0 or num > stk[-1]) and num > 0:
+            if (len(stk) == 0 or num > stk[-1]):
                 stk.append(num)
                 res += 1
         return res
