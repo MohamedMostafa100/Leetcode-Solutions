eec044a71ac26fa6e7372143a9525ed5519bca89
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-11-27 10:50:50 +0200

    Time: 14 ms (54.27%), Space: 171.2 MB (70.65%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -6,7 +6,7 @@ public:
         int l = 0;
         long long sum = 0;
         for(int r = 0; r < nums.size(); r++)
-        {
+        { 
             sum += (long long)nums[r];
             if(r - l + 1 < k)
             {
@@ -18,22 +18,22 @@ public:
                 sum -= (long long)nums[l];
                 l++;
             }
-        }
-        for(int i = k - 1; i < nums.size(); i++)
-        {
-            long long prev = 0;
-            if(i - k >= k - 1)
+            if(r >= k - 1)
             {
-                prev = pre[i - k];
-            }
-            res = max(res, pre[i] + prev);
-            if(pre[i] + prev < 0)
-            {
-                pre[i] = 0;
-            }
-            else
-            {
-                pre[i] += prev;
+                long long prev = 0;
+                if(r - k >= k - 1)
+                {
+                    prev = pre[r - k];
+                }
+                res = max(res, pre[r] + prev);
+                if(pre[r] + prev < 0)
+                {
+                    pre[r] = 0;
+                }
+                else
+                {
+                    pre[r] += prev;
+                }
             }
         }
         return res;
