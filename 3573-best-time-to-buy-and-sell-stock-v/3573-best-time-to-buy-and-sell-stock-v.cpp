ce389da351e3cbb780efce43ec435a7bd58bea02
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-12-18 02:23:39 +0200

    Time: 977 ms (17.49%), Space: 344.7 MB (26.89%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-    int n, K;
-    vector<int> prices;
-    long long dp[1005][505][3];
-
-    long long dfs(int i, int t, int state) {
-        if (i == n) {
-            return (state == 0 ? 0 : LLONG_MIN / 2);
-        }
-
-        if (dp[i][t][state] != -1)
-            return dp[i][t][state];
-
-        long long ans = dfs(i + 1, t, state);
-
-        if (state == 0) {
-            ans = max(ans, dfs(i + 1, t, 1) - prices[i]);
-            ans = max(ans, dfs(i + 1, t, 2) + prices[i]);
-        }
-        else if (state == 1 && t < K) {
-            ans = max(ans, dfs(i + 1, t + 1, 0) + prices[i]);
-        }
-        else if (state == 2 && t < K) {
-            ans = max(ans, dfs(i + 1, t + 1, 0) - prices[i]);
-        }
-
-        return dp[i][t][state] = ans;
-    }
-
     long long maximumProfit(vector<int>& prices, int k) {
-        this->prices = prices;
-        n = prices.size();
-        K = k;
-
-        memset(dp, -1, sizeof(dp));
-        return dfs(0, 0, 0);
+        vector<vector<vector<long long>>> dp(prices.size(), vector<vector<long long>>(k + 1, vector<long long>(3, -1)));
+        return solve(prices, dp, 0, k, 0);
+    }
+private:
+    long long solve(vector<int>& prices, vector<vector<vector<long long>>>& dp, int i, int k, int s)
+    {
+        if(i == prices.size())
+        {
+            if(s == 0)
+            {
+                return 0;
+            }
+            return LLONG_MIN / 2;
+        }
+        if(dp[i][k][s] == -1)
+        {
+            dp[i][k][s] = LLONG_MIN;
+            dp[i][k][s] = max(dp[i][k][s], solve(prices, dp, i + 1, k, s));
+            if(k > 0)
+            {
+                if(s == 0)
+                {
+                    dp[i][k][s] = max(dp[i][k][s], solve(prices, dp, i + 1, k, 1) - prices[i]);
+                    dp[i][k][s] = max(dp[i][k][s], solve(prices, dp, i + 1, k, 2) + prices[i]);
+                }
+                else if(s == 1)
+                {
+                    dp[i][k][s] = max(dp[i][k][s], solve(prices, dp, i + 1, k - 1, 0) + prices[i]);
+                }
+                else
+                {
+                    dp[i][k][s] = max(dp[i][k][s], solve(prices, dp, i + 1, k - 1, 0) - prices[i]);
+                }
+            }
+        }
+        return dp[i][k][s];
     }
 };
