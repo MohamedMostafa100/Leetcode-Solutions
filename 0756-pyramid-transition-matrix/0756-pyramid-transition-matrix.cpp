1b94be11558ab64334f3fa9ccf37b584dc7bf4b8
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2025-12-29 10:40:08 +0200

    Time: 42 ms (92.97%), Space: 13.1 MB (65.95%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
@@ -7,26 +7,22 @@ public:
         {
             pattern[allowed[i].substr(0, 2)].push_back(allowed[i][2]);
         }
-        return solve(bottom, "", pattern, "", visited, 0);
+        return solve(bottom, "", pattern, visited, 0);
     }
 private:
-    bool solve(string bottom, string top, unordered_map<string, vector<char>>& pattern, string curStr, unordered_set<string>& visited, int i)
+    bool solve(string bottom, string top, unordered_map<string, vector<char>>& pattern, unordered_set<string>& visited, int i)
     {
         if(bottom.length() == 1)
         {
             return true;
         }
-        if(visited.find(curStr) != visited.end())
+        if(visited.find(top) != visited.end())
         {
             return false;
         }
         if(i == bottom.length() - 1)
         {
-            if(solve(top, "", pattern, "", visited, 0))
-            {
-                return true;
-            }
-            return false;
+            return solve(top, "", pattern, visited, 0);
         }
         string block = bottom.substr(i, 2);
         if(pattern.find(block) != pattern.end())
@@ -34,7 +30,7 @@ private:
             for(int idx = 0; idx < pattern[block].size(); idx++)
             {
                 top += pattern[block][idx];
-                if(solve(bottom, top, pattern, curStr + pattern[block][idx], visited, i + 1))
+                if(solve(bottom, top, pattern, visited, i + 1))
                 {
                     return true;
                 }
