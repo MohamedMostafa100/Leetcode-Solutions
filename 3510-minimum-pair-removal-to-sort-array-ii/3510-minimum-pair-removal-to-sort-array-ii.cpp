849ebcf98e11f5c6f7d0c25929965be61fb364bd
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-01-25 03:37:42 +0200

    Time: 1822 ms (5.01%), Space: 273.4 MB (57.16%) - LeetHub

--- 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.cpp
+++ 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.cpp
@@ -1,115 +1,61 @@
-typedef long long ll;
-
-const int MAX_N = 100005;
-
-struct Node {
-    ll value;
-    int left;
-};
-
-using ListIt = std::list<Node>::iterator;
-
-struct Pair {
-    ListIt first;
-    ListIt second;
-    ll cost;
-    size_t firstLeft;
-    size_t secondLeft;
-
-    Pair() {}
-    Pair(ListIt fi, ListIt se, ll cost)
-        : first(fi),
-          second(se),
-          firstLeft(fi->left),
-          secondLeft(se->left),
-          cost(cost) {}
-};
-
-struct ComparePair {
-    bool operator()(const Pair& a, const Pair& b) {
-        if (a.cost != b.cost) {
-            return a.cost > b.cost;
-        }
-        return a.firstLeft > b.firstLeft;
-    }
-};
-
 class Solution {
 public:
-    int minimumPairRemoval(std::vector<int>& nums) {
-        std::list<Node> list;
-        std::bitset<MAX_N> merged;
-        std::priority_queue<Pair, std::vector<Pair>, ComparePair> pq;
-
-        int decreaseCount = 0;
-        int count = 0;
-
-        list.push_back({nums[0], 0});
-
-        for (size_t i = 1; i < nums.size(); ++i) {
-            list.push_back({nums[i], (int)i});
-
-            auto curr = std::prev(list.end());
-            auto prev = std::prev(curr);
-
-            pq.push({prev, curr, prev->value + curr->value});
-
-            if (nums[i - 1] > nums[i]) {
-                decreaseCount++;
+    int minimumPairRemoval(vector<int>& nums) {
+        int res = 0;
+        int errs = 0;
+        map<int, long long> arr;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
+                       greater<pair<long long, int>>>
+            pq;
+        unordered_set<int> erroredIdxs;
+        for (int i = 0; i < nums.size(); i++) {
+            arr[i] = nums[i];
+            if (i < nums.size() - 1) {
+                pq.push({(long long)nums[i] + nums[i + 1], i});
+            }
+            if (i > 0 && nums[i] < nums[i - 1]) {
+                erroredIdxs.insert(i - 1);
             }
         }
-
-        while (decreaseCount > 0 && !pq.empty()) {
-            auto top = pq.top();
+        while (!erroredIdxs.empty()) {
+            res++;
+            while (!pq.empty()) {
+                int idx = pq.top().second;
+                long long sum = pq.top().first;
+                auto it = arr.find(idx);
+                if (it == arr.end() || it == next(arr.end(), -1) ||
+                    arr[idx] + arr[next(it, 1)->first] != sum) {
+                    pq.pop();
+                } else {
+                    break;
+                }
+            }
+            int idx = pq.top().second;
+            long long sum = pq.top().first;
             pq.pop();
-
-            if (merged[top.firstLeft] || merged[top.secondLeft]) {
-                continue;
-            }
-
-            auto first = top.first;
-            auto second = top.second;
-            auto cost = top.cost;
-
-            if (first->value + second->value != cost) {
-                continue;
-            }
-
-            count++;
-
-            if (first->value > second->value) {
-                decreaseCount--;
-            }
-
-            ListIt prev =
-                (first == list.begin()) ? list.end() : std::prev(first);
-            ListIt next = std::next(second);
-
-            if (prev != list.end()) {
-                if (prev->value > first->value && prev->value <= cost) {
-                    decreaseCount--;
+            auto it = arr.find(idx);
+            int nextIdx = next(it, 1)->first;
+            arr[idx] = sum;
+            erroredIdxs.erase(idx);
+            erroredIdxs.erase(nextIdx);
+            arr.erase(nextIdx);
+            if (it != next(arr.end(), -1)) {
+                nextIdx = next(it, 1)->first;
+                if (arr[idx] > arr[nextIdx]) {
+                    erroredIdxs.insert(idx);
                 }
-                if (prev->value <= first->value && prev->value > cost) {
-                    decreaseCount++;
-                }
-                pq.push({prev, first, prev->value + cost});
+                pq.push({arr[idx] + arr[nextIdx], idx});
             }
-
-            if (next != list.end()) {
-                if (second->value > next->value && cost <= next->value) {
-                    decreaseCount--;
+            if (it != arr.begin()) {
+                int prevIdx = next(it, -1)->first;
+                if (arr[prevIdx] > arr[idx]) {
+                    erroredIdxs.insert(prevIdx);
+                } else {
+                    erroredIdxs.erase(prevIdx);
                 }
-                if (second->value <= next->value && cost > next->value) {
-                    decreaseCount++;
-                }
-                pq.push({first, next, cost + next->value});
+                pq.push({arr[idx] + arr[prevIdx], prevIdx});
             }
-
-            first->value = cost;
-            merged[second->left] = 1;
-            list.erase(second);
         }
-
-        return count;
+        return res;
     }
 };
