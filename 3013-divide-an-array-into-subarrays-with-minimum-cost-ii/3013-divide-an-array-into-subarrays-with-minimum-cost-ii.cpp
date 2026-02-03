678cb434d7844045773f9a6fb0e0e41cdd9271a6
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-02-03 04:04:58 +0200

    Time: 460 ms (29.23%), Space: 180.7 MB (29.74%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -1,79 +1,58 @@
-// two multisets maintain the first k smallest values
-class Container {
-public:
-    Container(int k) : k(k), sm(0) {}
-
-    // adjust the size of the ordered set to ensure that the first k smallest
-    // values are all in st1
-    void adjust() {
-        while (st1.size() < k && st2.size() > 0) {
-            int x = *(st2.begin());
-            st1.emplace(x);
-            sm += x;
-            st2.erase(st2.begin());
-        }
-        while (st1.size() > k) {
-            int x = *prev(st1.end());
-            st2.emplace(x);
-            st1.erase(prev(st1.end()));
-            sm -= x;
-        }
-    }
-
-    // insert element x
-    void add(int x) {
-        if (!st2.empty() && x >= *(st2.begin())) {
-            st2.emplace(x);
-        } else {
-            st1.emplace(x);
-            sm += x;
-        }
-        adjust();
-    }
-
-    // delete element x
-    void erase(int x) {
-        auto it = st1.find(x);
-        if (it != st1.end()) {
-            st1.erase(it), sm -= x;
-        } else {
-            st2.erase(st2.find(x));
-        }
-        adjust();
-    }
-
-    // sum of the first k smallest elements
-    long long sum() { return sm; }
-
-private:
-    int k;
-    // st1 saves the k smallest values, st2 saves the other values
-    multiset<int> st1, st2;
-    // sm represents the sum of the first k smallest elements
-    long long sm;
-};
-
 class Solution {
 public:
     long long minimumCost(vector<int>& nums, int k, int dist) {
+        long long res = LLONG_MAX;
         int n = nums.size();
-        // sliding window initialization
-        Container cnt(k - 2);
-        for (int i = 1; i < k - 1; i++) {
-            cnt.add(nums[i]);
-        }
-
-        long long ans = cnt.sum() + nums[k - 1];
-        // enumerate the beginning of the last array
-        for (int i = k; i < n; i++) {
-            int j = i - dist - 1;
-            if (j > 0) {
-                cnt.erase(nums[j]);
+        int l = 2;
+        int r = 2;
+        multiset<int> window;
+        multiset<int> smallest;
+        long long windowSum = nums[l];
+        smallest.insert(nums[l]);
+        for(int i = 1; i < n - k + 2; i++)
+        { 
+            while(r - l + 1 < min(dist, n - i - 1))
+            {
+                r++;
+                if(smallest.size() < k - 2)
+                {
+                    smallest.insert(nums[r]);
+                    windowSum += nums[r];
+                }
+                else
+                {
+                    auto lastEle = next(smallest.end(), -1);
+                    if(nums[r] < *lastEle)
+                    {
+                        windowSum += -*lastEle + nums[r];
+                        window.insert(*lastEle);
+                        smallest.erase(lastEle);
+                        smallest.insert(nums[r]);
+                    }
+                    else
+                    {
+                        window.insert(nums[r]);
+                    }
+                }
             }
-            cnt.add(nums[i - 1]);
-            ans = min(ans, cnt.sum() + nums[i]);
+            res = min(res, nums[0] + nums[i] + windowSum);
+            if(smallest.find(nums[l]) != smallest.end())
+            {
+                smallest.erase(smallest.find(nums[l]));
+                windowSum -= nums[l];
+                if(!window.empty())
+                {
+                    smallest.insert(*(window.begin()));
+                    windowSum += *(window.begin());
+                    window.erase(window.begin());
+                }
+            }
+            else
+            {
+                window.erase(window.find(nums[l]));
+            }
+            l++;
         }
-
-        return ans + nums[0];
+        return res;
     }
 };
