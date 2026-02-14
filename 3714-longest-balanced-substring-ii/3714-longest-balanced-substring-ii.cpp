05297ddb4ca7adae8ec47ce3f2b4f1c78153d5bb
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-02-14 03:27:50 +0200

    Time: 1747 ms (52.13%), Space: 493.7 MB (5.18%) - LeetHub

--- 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.cpp
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.cpp
@@ -1,81 +1,101 @@
 class Solution {
 public:
     int longestBalanced(string s) {
-        int results[7] = {
-            solve1(s, 'a'),
-            solve1(s, 'b'),
-            solve1(s, 'c'),
-            solve2(s, 'a', 'b'),
-            solve2(s, 'a', 'c'),
-            solve2(s, 'b', 'c'),
-            solve3(s)
-        };
-        int ans = 0;
-        for (int v : results) ans = max(ans, v);
-        return ans;
+        int res = 0;
+        res = max(res, case1(s, 'a'));
+        res = max(res, case1(s, 'b'));
+        res = max(res, case1(s, 'c'));
+        res = max(res, case2(s, 'a', 'b'));
+        res = max(res, case2(s, 'a', 'c'));
+        res = max(res, case2(s, 'b', 'c'));
+        res = max(res, case3(s));
+        return res;
     }
-
 private:
-    int solve1(const string& s, char t) {
-        int result = 0;
-        int count = 0;
-        for (char c : s) {
-            if (c == t) {
-                count++;
-                result = max(result, count);
-            } else {
-                count = 0;
+    int case1(string s, char x)
+    {
+        int res = 0;
+        for(int i = 0; i < s.length(); i++)
+        {
+            int cnt = 0;
+            while(i < s.length() && s[i] == x)
+            {
+                cnt++;
+                res = max(res, cnt);
+                i++;
             }
         }
-        return result;
+        return res;
     }
-
-    int solve2(const string& s, char t1, char t2) {
-        int result = 0;
-        int counts[2] = {0, 0};
-        unordered_map<int, int> previousPositions;
-        for (int i = 0; i < (int)s.size(); i++) {
-            char c = s[i];
-            if (c != t1 && c != t2) {
-                previousPositions.clear();
-                counts[0] = 0;
-                counts[1] = 0;
-            } else {
-                if (c == t1) counts[0]++; else counts[1]++;
-                if (counts[0] == counts[1]) {
-                    result = max(result, counts[0] * 2);
-                } else {
-                    int diff = counts[0] - counts[1];
-                    auto it = previousPositions.find(diff);
-                    if (it != previousPositions.end()) {
-                        result = max(result, i - it->second);
-                    } else {
-                        previousPositions[diff] = i;
-                    }
+    int case2(string s, char x, char y)
+    {
+        int res = 0;
+        for(int i = 0; i < s.length(); i++)
+        {
+            unordered_map<int, int> diff;
+            diff[0] = i - 1;
+            int xCnt = 0;
+            int yCnt = 0;
+            while(i < s.length() && (s[i] == x || s[i] == y))
+            {
+                if(s[i] == x)
+                {
+                    xCnt++;
                 }
+                else
+                {
+                    yCnt++;
+                }
+                int curDiff = xCnt - yCnt;
+                if(diff.count(curDiff))
+                {
+                    res = max(res, i - diff[curDiff]);
+                }
+                else
+                {
+                    diff[curDiff] = i;
+                }    
+                i++;
             }
         }
-        return result;
+        return res;
     }
-
-    int solve3(const string& s) {
-        int result = 0;
-        int counts[3] = {0, 0, 0};
-        unordered_map<long long, int> previousPositions;
-        for (int i = 0; i < (int)s.size(); i++) {
-            counts[s[i] - 'a']++;
-            if (counts[0] == counts[1] && counts[1] == counts[2]) {
-                result = i + 1;
-            } else {
-                long long diff = (long long)(counts[0] - counts[1]) * 100001LL + (long long)(counts[1] - counts[2]);
-                auto it = previousPositions.find(diff);
-                if (it != previousPositions.end()) {
-                    result = max(result, i - it->second);
-                } else {
-                    previousPositions[diff] = i;
-                }
+    int case3(string s)
+    {
+        int res = 0;
+        unordered_map<string, int> diff;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d1 = 0;
+        int d2 = 0;
+        diff["0,0"] = -1;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == 'a')
+            {
+                a++;
+            }
+            else if(s[i] == 'b')
+            {
+                b++;
+            }
+            else
+            {
+                c++;
+            }
+            d1 = a - b;
+            d2 = a - c;
+            string hash = to_string(d1) + "," + to_string(d2);
+            if(diff.count(hash))
+            {
+                res = max(res, i - diff[hash]);
+            }
+            else
+            {
+                diff[hash] = i;
             }
         }
-        return result;
+        return res;
     }
 };
