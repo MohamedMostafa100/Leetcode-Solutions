85500fde0f178468c83bb9c325d4b9e77ffe0a80
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-02-18 02:12:45 +0200

    Time: 1 ms (25.94%), Space: 9.7 MB (65.72%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.cpp
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
     vector<string> readBinaryWatch(int turnedOn) {
-        vector<string> ans;
-        for (int h = 0; h < 12; ++h) {
-            for (int m = 0; m < 60; ++m) {
-                if (__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
-                    ans.push_back(to_string(h) + ":" + (m < 10 ? "0" : "") +
-                                  to_string(m));
+        vector<string> res;
+        int hoursMask = 960;
+        int minsMask = 63;
+        for(int i = 1; i <= 1024; i++)
+        {
+            if(countBits(i) == turnedOn)
+            {
+                int h = (hoursMask & i) >> 6;
+                int m = minsMask & i;
+                if(h > 11 || m > 59)
+                {
+                    continue;
                 }
+                res.push_back(to_string(h) + ":" + (m < 10 ? "0" : "") + to_string(m));
             }
         }
-        return ans;
+        return res;
+    }
+private:
+    char countBits(int n)
+    {
+        char bits = 0;
+        int mask = 1;
+        for(int i = 0; i < 10; i++)
+        {
+            if(mask & n)
+            {
+                bits++;
+            }
+            mask <<= 1;
+        }
+        return bits;
     }
 };
