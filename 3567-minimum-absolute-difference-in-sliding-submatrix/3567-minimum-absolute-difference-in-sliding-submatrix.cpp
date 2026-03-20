92b638622e1ab81b55699042b3e8af946d00ad47
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-03-20 23:07:02 +0200

    Time: 19 ms (57.86%), Space: 32.2 MB (57.14%) - LeetHub

--- 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -6,19 +6,19 @@ public:
         vector<vector<int>> res(m - k + 1, vector<int>(n - k + 1));
         for (int i = 0; i + k <= m; i++) {
             for (int j = 0; j + k <= n; j++) {
-                vector<int> kgrid;
+                vector<int> subGrid;
                 for (int x = i; x < i + k; x++) {
                     for (int y = j; y < j + k; y++) {
-                        kgrid.push_back(grid[x][y]);
+                        subGrid.push_back(grid[x][y]);
                     }
                 }
                 int kmin = INT_MAX;
-                sort(kgrid.begin(), kgrid.end());
-                for (int t = 1; t < kgrid.size(); t++) {
-                    if (kgrid[t] == kgrid[t - 1]) {
+                sort(subGrid.begin(), subGrid.end());
+                for (int t = 1; t < subGrid.size(); t++) {
+                    if (subGrid[t] == subGrid[t - 1]) {
                         continue;
                     }
-                    kmin = min(kmin, kgrid[t] - kgrid[t - 1]);
+                    kmin = min(kmin, subGrid[t] - subGrid[t - 1]);
                 }
                 if (kmin != INT_MAX) {
                     res[i][j] = kmin;
