4cf1ba277e880fbee353cb9eca5bf6e4d629fff7
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-03-30 02:57:56 +0200

    Time: 3 ms (92.53%), Space: 19.8 MB (62.64%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        vector<int> even1(26, 0);
+        vector<int> even2(26, 0);
+        vector<int> odd1(26, 0);
+        vector<int> odd2(26, 0);
+        for(int i = 0; i < s1.length(); i++)
+        {
+            if(i % 2 == 0)
+            {
+                even1[s1[i] - 'a']++;
+                even2[s2[i] - 'a']++;
+            }
+            else
+            {
+                odd1[s1[i] - 'a']++;
+                odd2[s2[i] - 'a']++;
+            }
+        }
+        for(int i = 0; i < 26; i++)
+        {
+            if(even1[i] != even2[i] || odd1[i] != odd2[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
