ea1880c2ec7151032217a5b837047af0e70dbc85
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-04-02 11:25:24 +0200

    Time: 243 ms (14.14%), Space: 272.4 MB (8.77%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,52 +1,54 @@
 class Solution {
 public:
-    vector<int> survivedRobotsHealths(vector<int>& positions,
-                                      vector<int>& healths, string directions) {
-        int n = positions.size();
-        vector<int> indices(n), result;
-        stack<int> stack;
-
-        for (int index = 0; index < n; ++index) {
-            indices[index] = index;
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        vector<int> res;
+        vector<vector<int>> robots;
+        stack<vector<int>> stk;
+        for(int i = 0; i < positions.size(); i++)
+        {
+            robots.push_back({positions[i], healths[i], directions[i] == 'R' ? 0 : 1, i});
         }
-
-        sort(indices.begin(), indices.end(),
-             [&](int lhs, int rhs) { return positions[lhs] < positions[rhs]; });
-
-        for (int currentIndex : indices) {
-            // Add right-moving robots to the stack
-            if (directions[currentIndex] == 'R') {
-                stack.push(currentIndex);
-            } else {
-                while (!stack.empty() && healths[currentIndex] > 0) {
-                    // Pop the top robot from the stack for collision check
-                    int topIndex = stack.top();
-                    stack.pop();
-
-                    // Top robot survives, current robot is destroyed
-                    if (healths[topIndex] > healths[currentIndex]) {
-                        healths[topIndex] -= 1;
-                        healths[currentIndex] = 0;
-                        stack.push(topIndex);
-                    } else if (healths[topIndex] < healths[currentIndex]) {
-                        // Current robot survives, top robot is destroyed
-                        healths[currentIndex] -= 1;
-                        healths[topIndex] = 0;
-                    } else {
-                        // Both robots are destroyed
-                        healths[currentIndex] = 0;
-                        healths[topIndex] = 0;
-                    }
+        sort(robots.begin(), robots.end());
+        for(int i = 0; i < robots.size(); i++)
+        {
+            if(robots[i][2] == 0)
+            {
+                stk.push(robots[i]);
+            }
+            else
+            {
+                while(!stk.empty() && stk.top()[2] == 0 && stk.top()[1] < robots[i][1])
+                {
+                    healths[stk.top()[3]] = 0;
+                    healths[robots[i][3]]--;
+                    robots[i][1]--;
+                    stk.pop();
+                }
+                if(stk.empty() || stk.top()[2] == 1)
+                {
+                    stk.push(robots[i]);
+                }
+                else if(stk.top()[1] > robots[i][1])
+                {
+                    stk.top()[1]--;
+                    healths[stk.top()[3]]--;
+                    healths[robots[i][3]] = 0;
+                }
+                else
+                {
+                    healths[stk.top()[3]] = 0;
+                    healths[robots[i][3]] = 0;
+                    stk.pop();
                 }
             }
         }
-
-        // Collect surviving robots
-        for (int index = 0; index < n; ++index) {
-            if (healths[index] > 0) {
-                result.push_back(healths[index]);
+        for(int i = 0; i < healths.size(); i++)
+        {
+            if(healths[i] > 0)
+            {
+                res.push_back(healths[i]);
             }
         }
-        return result;
-    }
+        return res;
+    } 
 };
