8336f288c0af84e84e901d110be4f8472ad3c7e0
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-04-05 01:11:08 +0200

    Time: 239 ms (43.5%), Space: 27.5 MB (75.97%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
@@ -4,12 +4,11 @@ class Solution:
         cols = len(encodedText) // rows
         if cols == 0:
             return ""
-        textMat = [list(encodedText[i * cols : i * cols + cols]) for i in range(rows)]
         for idx in range(cols):
             i = 0
             j = idx
             while i < rows and j < cols:
-                res += textMat[i][j]
+                res += encodedText[i * cols + j]
                 i += 1
                 j += 1
         return res.rstrip()
