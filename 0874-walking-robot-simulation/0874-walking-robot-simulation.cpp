f90e1c387438724cd8f4bbbadcac82afd652df59
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-04-06 22:24:14 +0200

    Time: 23 ms (66.76%), Space: 36.9 MB (87.13%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -8,7 +8,7 @@ public:
         unordered_set<int> obs;
         vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         for (int i = 0; i < obstacles.size(); i++) {
-            obs.insert(obstacles[i][0] * 60013 + obstacles[i][1]);
+            obs.insert(obstacles[i][0] + 60013 * obstacles[i][1]);
         }
         for (int i = 0; i < commands.size(); i++) {
             if (commands[i] == -1) {
@@ -18,12 +18,11 @@ public:
             } else if (commands[i] == -2) {
                 curDir--;
                 curDir = ((curDir % 4) + 4) % 4;
-                cout << curDir << endl;
                 continue;
             }
             for(int j = 0; j < commands[i]; j++)
             {
-                if(obs.count((x + dirs[curDir].first) * 60013 + (y + dirs[curDir].second)))
+                if(obs.count((x + dirs[curDir].first) + 60013 * (y + dirs[curDir].second)))
                 {
                     break;
                 }
