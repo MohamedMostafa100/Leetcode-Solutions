fd6a19a8fae6c7a8516fe9f2ded83881ab07e765
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-04-19 04:05:17 +0200

    Time: 55 ms (64.95%), Space: 36.3 MB (7.9%) - LeetHub

---
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        res = 0
+        i = len(nums1) - 1
+        j = len(nums2) - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] <= nums2[j]:
+                res = max(res, j - i)
+                i -= 1
+            else:
+                j -= 1
+        return res
