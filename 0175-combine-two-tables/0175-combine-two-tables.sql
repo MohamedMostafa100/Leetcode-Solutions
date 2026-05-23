011650a29f24ca34d5d13b7254a4da230114e465
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-05-24 00:55:36 +0300

    Time: 436 ms (53.87%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,4 @@
 /* Write your T-SQL query statement below */
-SELECT Person.firstName, Person.lastName, Address.city, Address.state
+SELECT firstName, lastName, city, state
 FROM Person
 LEFT JOIN Address ON Person.personId = Address.personId;
