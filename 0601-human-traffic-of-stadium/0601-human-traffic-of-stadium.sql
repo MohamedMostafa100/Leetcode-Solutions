25f3ea1b8eebe066d7d5f0c7a9810a38354837b2
Mohamed Mostafa <89148745+MohamedMostafa100@users.noreply.github.com>
2026-05-29 14:54:18 +0300

    Time: 334 ms (37.98%), Space: 0B (100%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,17 @@
+/* Write your T-SQL query statement below */
+WITH GroupFilterCTE AS(
+    -- rows with equal groupID are consecutive
+    SELECT *, id - ROW_NUMBER() OVER(ORDER BY id) groupID
+    FROM Stadium
+    WHERE people >= 100
+)
+
+SELECT id, visit_date, people
+FROM GroupFilterCTE
+WHERE groupID IN (
+    SELECT groupID
+    FROM GroupFilterCTE
+    GROUP BY groupID
+    HAVING COUNT(*) >=3
+)
+ORDER BY visit_date ASC
